0a3942f8f75f6bbc515d84f2a312ad8cf02372d4
alanlin-cpu <alan66333090@gmail.com>
2025-12-17 18:37:00 +0000

    初始化

--- damdamzyun/vite.config.js
+++ damdamzyun/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/OrderSystem/',
+  base: '/damdamzyun/',
   plugins: [react()],
   server: {
     middlewareMode: false,
